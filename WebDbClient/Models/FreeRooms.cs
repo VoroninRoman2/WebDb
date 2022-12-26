bf950ff476595ba72fd4b7d9dd139e0ea18e181b
RomanVor <roma2003voronin@gmail.com>
2022-12-26 19:16:16 +0200

    Models created

---
+++ WebDbClient/Models/FreeRooms.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WbDbServer.Models
+{
+    internal class FreeRooms
+    {
+        public int Id { get; set; }
+        public int HotelId { get; set; }
+        public int RoomNumber { get; set; }
+        public int Price { get; set; }
+        public FreeRooms(int id,int hotelId,int rooms, int price)
+        {
+            Id= id;
+            HotelId = hotelId;
+            RoomNumber = rooms;
+            Price = price;
+        }
+        public FreeRooms(int hotelId, int rooms, int price)
+        {
+            HotelId = hotelId;
+            RoomNumber = rooms;
+            Price = price;
+        }
+
+    }
+}
